9f08a28f92661a160cf122ab94a4832c9e6e8db1
Smith-Jefferson <zhishanghan@qq.com>
2019-01-09 19:28:27 +0800

    Update UsdtServer.java

--- src/main/java/org/aminism/spider/huobi/server/UsdtServer.java
+++ src/main/java/org/aminism/spider/huobi/server/UsdtServer.java
@@ -1,11 +1,13 @@
 package org.aminism.spider.huobi.server;
 
+import org.aminism.spider.huobi.common.MailCaller;
 import org.aminism.spider.huobi.dao.HuobiTradeProductDao;
 import org.aminism.spider.huobi.entity.HuobiTradeProduct;
 import org.aminism.spider.huobi.usdt.UsdtSpider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -17,12 +19,32 @@ public class UsdtServer {
     UsdtSpider spider;
     @Autowired
     HuobiTradeProductDao dao;
+    @Autowired
+    MailCaller mailCaller;
+
+
 
     public void collectInfo(){
         List<HuobiTradeProduct> list = spider.task();
-        if(list !=null && !list.isEmpty()){
-            dao.save(list);
-            dao.flush();
+        if(list ==null || list.isEmpty()){
+            return;
+        }
+        HuobiTradeProduct p = list.stream().min(Comparator.comparing(HuobiTradeProduct::getPrice)).get();
+        Double avg = list.stream().mapToDouble(HuobiTradeProduct::getPrice).average().getAsDouble();
+        p.setAvgPrice(avg);
+        dao.saveAndFlush(p);
+    }
+
+    public void sendMail(HuobiTradeProduct p){
+        HuobiTradeProduct v = dao.findFirstByOrderByDatachange_lasttimeDesc();
+        if(v==null || !v.getPrice().equals(p.getPrice()) || !v.getAvgPrice().equals(p.getAvgPrice())){
+            StringBuilder builder = new StringBuilder();
+            //todo
+            try {
+                mailCaller.sendMail(builder.toString(),"w952577382@126.com");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
