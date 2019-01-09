498366a18a4bbabef11109bd33bd7fc26326cba1
Smith-Jefferson <zhishanghan@qq.com>
2019-01-09 19:29:09 +0800

    Update HuobiTradeProductDao.java

--- src/main/java/org/aminism/spider/huobi/dao/HuobiTradeProductDao.java
+++ src/main/java/org/aminism/spider/huobi/dao/HuobiTradeProductDao.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface HuobiTradeProductDao extends JpaRepository<HuobiTradeProduct,Long> {
-
+    HuobiTradeProduct findFirstByOrderByDatachange_lasttimeDesc();
 }
